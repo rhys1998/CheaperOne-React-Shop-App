9302e776a9ed6ab6c0551b72e9b650513f5183e4
rhys1998 <theroyalrajesh7@gmail.com>
2020-08-21 21:11:20 +0530

    productio

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ const config = require("./config/key");
 //   .catch(err => console.error(err));
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+const connect = mongoose.connect('mongodb+srv://me_rajesh:Rajesh@123@cluster0.ljion.mongodb.net/<dbname>?retryWrites=true&w=majority'
+, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
