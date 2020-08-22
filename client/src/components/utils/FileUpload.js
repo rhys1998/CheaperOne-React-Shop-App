1e96235020f51989e4e4e6f7b5b8d98faeaa1609
rhys1998 <theroyalrajesh7@gmail.com>
2020-08-22 20:53:39 +0530

    upload into AWS

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import Dropzone from 'react-dropzone';
 import { Icon } from 'antd';
 import Axios from 'axios';
+import S3 from 'react-aws-s3';
+
 function FileUpload(props) {
 
     const [Images, setImages] = useState([])
@@ -12,19 +14,42 @@ function FileUpload(props) {
         const config = {
             header: { 'content-type': 'multipart/form-data' }
         }
+
+        const config2 = {
+            bucketName: 'cheaperone',
+            dirName: 'media', /* optional */
+            region: 'ap-south-1',
+            accessKeyId: 'AKIAJ4QVQCNWXRBCIJGA',
+            secretAccessKey: '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',
+        }
+
+
+        const ReactS3Client = new S3(config2);
+ 
+        const newFileName = 'test-file';
+ 
+ReactS3Client
+    .uploadFile(files[0], newFileName)
+    .then(data => {
+        setImages([...Images, data.location])
+        props.refreshFunction([...Images, data.location])
+    })
+    .catch(err => console.error(err, "error"))
+ 
+  /**
+   * {
+   *   Response: {
+   *     bucket: "myBucket",
+   *     key: "image/test-image.jpg",
+   *     location: "https://myBucket.s3.amazonaws.com/media/test-file.jpg"
+   *   }
+   * }
+   */
+
+
+
         formData.append("file", files[0])
-        //save the Image we chose inside the Node Server 
-        Axios.post('/api/product/uploadImage', formData, config)
-            .then(response => {
-                if (response.data.success) {
-
-                    setImages([...Images, response.data.image])
-                    props.refreshFunction([...Images, response.data.image])
-
-                } else {
-                    alert('Failed to save the Image in Server')
-                }
-            })
+    
     }
 
 
@@ -52,8 +77,6 @@ function FileUpload(props) {
                     }}
                         {...getRootProps()}
                     >
-                        {console.log('getRootProps', { ...getRootProps() })}
-                        {console.log('getInputProps', { ...getInputProps() })}
                         <input {...getInputProps()} />
                         <Icon type="plus" style={{ fontSize: '3rem' }} />
 
@@ -62,10 +85,11 @@ function FileUpload(props) {
             </Dropzone>
 
             <div style={{ display: 'flex', width: '350px', height: '240px', overflowX: 'scroll' }}>
-
-                {Images.map((image, index) => (
+                
+                {
+                Images.map((image, index) => (  
                     <div onClick={() => onDelete(image)}>
-                        <img style={{ minWidth: '300px', width: '300px', height: '240px' }} src={`http://localhost:5000/${image}`} alt={`productImg-${index}`} />
+                        <img style={{ minWidth: '300px', width: '300px', height: '240px' }} src={image} alt={`productImg-${index}`} />
                     </div>
                 ))}
 
