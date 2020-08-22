f42b4c3403c229cbc10b0dee55e03fd699d728be
rhys1998 <theroyalrajesh7@gmail.com>
2020-08-22 21:00:43 +0530

    blank removed in footer

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
             flexDirection: 'column', alignItems: 'center',
             justifyContent: 'center', fontSize:'1rem'
         }}>
-           <a href = "https://mail.google.com/mail/?view=cm&fs=1&to=i.r.rakshit@gmail.com" target='_blank'>
+           <a href = "https://mail.google.com/mail/?view=cm&fs=1&to=i.r.rakshit@gmail.com" >
                     Send Feedback
             </a>
         </div>
